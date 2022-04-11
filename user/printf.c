1c16a04b2692ae7c91c393f912d6e0f85730d9cb
wsmbsbbz <1037214973@qq.com>
2022-04-11 23:40:10 +0800

    Uncompleted lab: Backtrace. make error: undefined reference to 'backtrace'

--- user/printf.c
+++ user/printf.c
@@ -1,5 +1,6 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/riscv.h"
 #include "user/user.h"
 
 #include <stdarg.h>
@@ -110,4 +111,18 @@ printf(const char *fmt, ...)
 
   va_start(ap, fmt);
   vprintf(1, fmt, ap);
+}
+
+void
+backtrace(void)
+{
+  uint64 fp, pvm_top, r_adr;
+  printf("backtrace:\n");
+  fp = r_fp();
+  pvm_top = PGROUNDUP(fp);
+  while (fp < pvm_top) {
+    r_adr = *(uint64 *)(fp - 8);
+    printf("%p\n", r_adr);
+    fp = *(uint64 *)(fp - 16);
+  }
 }
